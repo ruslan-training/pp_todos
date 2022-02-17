00c3211fbdb30973236c40dab205af59b6bd8fef
kbnd <hidrolyz@gmail.com>
2022-02-17 03:01:51 +0200

    123

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="app" v-cloak>
+    <div class="app__wrapper">
+      <h1>TODOS list</h1>
+      <vToDo></vToDo>
+    </div>
+  </div>
+</template>
+
+<script>
+import vToDo from './components/v-todos.vue'
+
+export default {
+  name: 'App',
+  components: {
+    vToDo
+  }
+}
+</script>
+
+<style lang="scss">
+body {
+  background-color: #eee;
+}
+  .app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    padding-top: 15px;
+
+    h1 {
+      color: $mainColor;
+      font-size: 1.5em; 
+      margin-bottom: 1em; 
+    }
+    &__wrapper {
+      max-width: 500px;
+      margin: 0 auto;
+    }
+  }
+  [v-cloak] {
+    display: none;
+  }
+</style>
